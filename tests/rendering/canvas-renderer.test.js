feb2df2070a098fcf302024fef9f9f58b39467c9
jaenyf <jaenyf@protonmail.com>
2025-11-23 11:55:52 +0100

    Cleanup canvas-renderer tests and add nodes/edges visibility tests

--- tests/rendering/canvas-renderer.test.js
+++ tests/rendering/canvas-renderer.test.js
@@ -35,9 +35,7 @@ describe("drawGraph", () => {
         expect(context.clearRect).toHaveBeenCalled();
         expect(context.drawImage).toHaveBeenCalled();
     });
-});
 
-describe("drawGraph", () => {
     it("draw edges", () => {
         // Arrange
         const state = createGraphState();
@@ -71,9 +69,61 @@ describe("drawGraph", () => {
         expect(cachedContext.lineTo).toHaveBeenCalledTimes(1);
         expect(cachedContext.stroke).toHaveBeenCalledTimes(1);
     });
-});
 
-describe("drawGraph", () => {
+    it.each([
+        {
+            nodes: [
+                { x: -1000, y: -1000 }, // outside viewport
+                { x: 10000, y: 10000 } // outside viewport
+            ],
+            expectedEdgesDrawed: 1  //but edge cross the viewport
+        },
+        {
+            nodes: [
+                { x: -1000, y: -1000 }, // outside viewport
+                { x: -10000, y: 10000 } // outside viewport
+            ],
+            expectedEdgesDrawed: 0  //edge does not cross the viewport
+        },
+        {
+            nodes: [
+                { x: 1000, y: -1000 }, // outside viewport
+                { x: 10000, y: 10000 } // outside viewport
+            ],
+            expectedEdgesDrawed: 0  //edge does not cross the viewport
+        }
+    ])("draw visible edges", (testArg) => {
+        // Arrange
+        const state = createGraphState();
+        const mockedUI = queryUIMock(state);
+        const canvas = mockedUI.canvas;
+        const context = canvas.getContext("2d");
+
+        const cachedCanvas = createMockCanvas();
+        globalThis.document.createElement = (tagName) => {
+            if (tagName.toLowerCase() === "canvas") {
+                return cachedCanvas;
+            }
+        }
+
+        state.nodes = testArg.nodes;
+
+        state.edges = [
+            { from: state.nodes[0], to: state.nodes[1] }
+        ];
+
+        resetCanvasCache();
+
+        // Act
+        drawGraph(context, canvas, state);
+
+        // Assert
+        // The edge crossing the viewport should have been drawed
+        const cachedContext = cachedCanvas.getContext("2d");
+        expect(cachedContext.lineTo).toHaveBeenCalledTimes(testArg.expectedEdgesDrawed);
+        expect(cachedContext.stroke).toHaveBeenCalledTimes(testArg.expectedEdgesDrawed);
+    });
+
     it("draw nodes", () => {
         // Arrange
         const state = createGraphState();
@@ -107,9 +157,82 @@ describe("drawGraph", () => {
         expect(cachedContext.arc).toHaveBeenCalledTimes(2);
         expect(cachedContext.fill).toHaveBeenCalledTimes(2);
     });
-});
 
-describe("drawGraph", () => {
+    it.each([
+        {
+            nodes: [
+                { x: 10, y: 10 }, // inside viewport
+                { x: 10000, y: 10000 } // outside viewport
+            ],
+            expectedNodesDrawed: 1
+        },
+        {
+            nodes: [
+                { x: -1000, y: -1000 }, // ouside viewport
+                { x: 10, y: 10 } // inside viewport
+            ],
+            expectedNodesDrawed: 1
+        },
+        {
+            nodes: [
+                { x: 10, y: 10 }, // inside viewport
+                { x: 30, y: 30 } // inside viewport
+            ],
+            expectedNodesDrawed: 2
+        },
+        {
+            nodes: [
+                { x: -1000, y: -1000 }, // outside viewport
+                { x: 10000, y: 10000 } // outside viewport
+            ],
+            expectedNodesDrawed: 0
+        },
+        {
+            nodes: [
+                { x: -1000, y: -1000 }, // outside viewport
+                { x: -10000, y: 10000 } // outside viewport
+            ],
+            expectedNodesDrawed: 0
+        },
+        {
+            nodes: [
+                { x: 1000, y: -1000 }, // outside viewport
+                { x: 10000, y: 10000 } // outside viewport
+            ],
+            expectedNodesDrawed: 0
+        }
+    ])("draw visible nodes", (testArg) => {
+        // Arrange
+        const state = createGraphState();
+        const mockedUI = queryUIMock(state);
+        const canvas = mockedUI.canvas;
+        const context = canvas.getContext("2d");
+
+        const cachedCanvas = createMockCanvas();
+        globalThis.document.createElement = (tagName) => {
+            if (tagName.toLowerCase() === "canvas") {
+                return cachedCanvas;
+            }
+        }
+
+        state.nodes = testArg.nodes;
+
+        state.edges = [
+            { from: state.nodes[0], to: state.nodes[1] }
+        ];
+
+        resetCanvasCache();
+
+        // Act
+        drawGraph(context, canvas, state);
+
+        // Assert
+        // The edge crossing the viewport should have been drawed
+        const cachedContext = cachedCanvas.getContext("2d");
+        expect(cachedContext.arc).toHaveBeenCalledTimes(testArg.expectedNodesDrawed);
+        expect(cachedContext.fill).toHaveBeenCalledTimes(testArg.expectedNodesDrawed);
+    });
+
     it("draw line of symmetry", () => {
         // Arrange
         const state = createGraphState();
